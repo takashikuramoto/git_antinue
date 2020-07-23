14436e1a1e28cda3b3651b661ee7b6c529117bd9
EC2 Default User <ec2-user@ip-172-31-6-26.ap-northeast-1.compute.internal>
2020-07-23 14:14:34 +0000

    top

---
+++ db/migrate/20200723135946_create_tops.rb
@@ -0,0 +1,10 @@
+class CreateTops < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tops do |t|
+      t.string      :name
+      t.text        :text
+      t.text        :image
+      t.timestamps
+    end
+  end
+end
